8d8b54c12432eff0badefff48c485d400d174779
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-08 18:18:41 +0900

    Issue4~6 の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,6 +6,8 @@ class CalendarsController < ApplicationController
     @plan = Plan.new
   end
 
+
+
   # 予定の保存
   def create
     Plan.create(plan_params)
@@ -34,7 +36,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => getweek}
       @week_days.push(days)
     end
 
